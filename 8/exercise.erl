c4f6af69b5abc8725ccd1363b6f9d0473a5211d7
y.zz <me@y.zz>
2015-07-21 14:36:11 +0800

    8.11-2

--- 8/exercise.erl
+++ 8/exercise.erl
@@ -33,3 +33,41 @@ wait()->
         true->io:format("bing~~~n");
         false->io:format("bang~~~n")
     end.
+
+
+
+% 8.11-2
+-compile(export_all).
+start(Num) ->
+    % cancel(),
+    unregister(circle_head),
+    register(circle_head, spawn(fun() -> create(Num)end)).
+
+
+send_message(N) ->
+    for(0, N, fun(_I) -> circle_head ! _I end).
+
+
+cancel(N) ->
+    for(0, N, fun(_I) -> circle_head ! cancel end).
+
+create(0) ->
+    self();
+create(Num) ->
+    Pid = spawn(fun() -> create(Num - 1) end),
+    io:format("self Pid:~p create process:~p~n", [self(), Pid]),
+    loop(Pid,Num).
+
+loop(Pid,Num) ->
+    receive
+        cancel ->
+            Pid ! cancel,
+            cancel;
+        Any ->
+            % io:format("Pid:~p receive:~p~n", [self(), Any]),
+            Pid ! Any,
+            io:format("ddd self:~p Pid:~p,create_at_num:~p receive:~p~n", [self(),Pid, Num,Any]),
+            loop(Pid,Num)
+    end.
+for(N,N,F)->[F(N)];
+for(I,N,F)->[F(I) | for(I + 1,N,F)].
